760d92ddfaa09b765117ce2db8669450bcc9e48b
hassan <hchoudhury20@hotmail.com>
2023-09-08 15:06:47 +0100

    add front end ui

---
+++ photo-tag/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './navbar'; // Assuming you've corrected the file name
+import './App.css';
+import Content from './content';
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    <Content />
+    </>
+  );
+}
+
+export default App;
---
+++ photo-tag/src/navbar.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+const navBar = () => {
+    return (
+      <nav className="navbar">wheres wally</nav>
+    );
+  };
+
+export default navBar
